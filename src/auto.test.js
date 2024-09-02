539de86ee595f4abfaaee5d1d9e80ea209db88fe
antonio <antonio.ontiveros@ucb.edu.bo>
2024-09-01 21:15:03 -0400

    Test 2: Mostrar la dimension del comando

--- src/auto.test.js
+++ src/auto.test.js
@@ -1,7 +1,10 @@
-import { leer_comando } from "./auto.js";
+import { leer_comando , leer_dimension} from "./auto.js";
 
 describe("Auto a Control Remoto", () => {
     it("deberia retornar el comando 5,5/1,2N/IAIAIAIAA", () => {
       expect(leer_comando("5,5/1,2N/IAIAIAIAA")).toEqual("5,5/1,2N/IAIAIAIAA");
+    });
+    it("deberia retornar la dimension del mapa", () => {
+      expect(leer_dimension("5,5/1,2N/IAIAIAIAA")).toEqual("5,5");
     });
   });
