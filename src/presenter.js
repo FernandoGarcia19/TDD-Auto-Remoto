59dc678add01869096af672ba2cd5856a9d8ef13
antonio <antonio.ontiveros@ucb.edu.bo>
2024-09-03 10:05:33 -0400

    UI: Validar comando

--- src/presenter.js
+++ src/presenter.js
@@ -7,13 +7,16 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const output = leer_comando(comando.value);
   const dimension = leer_dimension(comando.value);
   const pos_inicial = leer_pos_inicial(comando.value)
   const instrucciones = leer_instrucciones(comando.value)
-  div.innerHTML = 
-  "<p>Comando: " + output + "</p>" +
-  "<p>Dimension: " + dimension + "</p>" +
-  "<p>Posicion Inicial: " + pos_inicial + "</p>" +
-  "<p>Instrucciones: " + instrucciones + "</p>";
+  if(leer_comando(comando.value)){
+    div.innerHTML = 
+    "<p>Dimension: " + dimension + "</p>" +
+    "<p>Posicion Inicial: " + pos_inicial + "</p>" +
+    "<p>Instrucciones: " + instrucciones + "</p>";
+  }
+  else{
+    div.innerHTML = "<p>Comando inválido<p>"
+  }
 });
